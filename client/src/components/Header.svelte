155f28073a4c0a67c33e21f4079c8b0086e6eb6b
Maraş Deri <marasderi@gmail.com>
2025-08-07 10:22:44 +0300

    Create Header.svelte

---
+++ client/src/components/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let user;
+</script>
+
+<header class="bg-blue-600 text-white p-4">
+  <div class="container mx-auto flex justify-between items-center">
+    <h1 class="text-2xl font-bold">Kamu Mikroblog</h1>
+    <nav>
+      {#if user}
+        <a href="/profile" class="mr-4">{user.username}</a>
+        <a href="/logout" class="mr-4">Çıkış</a>
+      {:else}
+        <a href="/login" class="mr-4">Giriş</a>
+        <a href="/register">Kayıt Ol</a>
+      {/if}
+    </nav>
+  </div>
+</header>
