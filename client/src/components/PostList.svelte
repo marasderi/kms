d0abbff080982ca3423ff7709a8a506da941ae44
Maraş Deri <marasderi@gmail.com>
2025-08-07 11:57:31 +0300

    Update PostList.svelte

--- client/src/components/PostList.svelte
+++ client/src/components/PostList.svelte
@@ -1,15 +1,19 @@
 <script>
   import PostItem from './PostItem.svelte';
+  import { onMount } from 'svelte';
   let posts = [];
-  async function fetchPosts() {
-    const res = await fetch('/api/posts', { credentials: 'include' });
-    if (res.ok) posts = await res.json();
-  }
-  onMount(fetchPosts);
+  let user = null;
+
+  onMount(async () => {
+    const userRes = await fetch('/api/auth/me', { credentials: 'include' });
+    if (userRes.ok) user = await userRes.json();
+    const postRes = await fetch('/api/posts', { credentials: 'include' });
+    if (postRes.ok) posts = await postRes.json();
+  });
 </script>
 
 <div>
   {#each posts as post}
-    <PostItem {post} />
+    <PostItem {post} {user} />
   {/each}
 </div>
