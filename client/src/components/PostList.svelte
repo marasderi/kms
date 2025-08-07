01b94327ade6c9232fbd4a0676ecb50c02515784
Maraş Deri <marasderi@gmail.com>
2025-08-07 10:27:00 +0300

    Create PostList.svelte

---
+++ client/src/components/PostList.svelte
@@ -0,0 +1,15 @@
+<script>
+  import PostItem from './PostItem.svelte';
+  let posts = [];
+  async function fetchPosts() {
+    const res = await fetch('/api/posts', { credentials: 'include' });
+    if (res.ok) posts = await res.json();
+  }
+  onMount(fetchPosts);
+</script>
+
+<div>
+  {#each posts as post}
+    <PostItem {post} />
+  {/each}
+</div>
