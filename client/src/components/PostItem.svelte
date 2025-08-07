e309c38d9eaabc26b25356387c3179d250f6ca20
Maraş Deri <marasderi@gmail.com>
2025-08-07 10:26:44 +0300

    Create PostItem.svelte

---
+++ client/src/components/PostItem.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let post;
+</script>
+
+<div class="p-4 bg-white shadow rounded mb-4">
+  <p class="font-bold">{post.user.username}</p>
+  <p>{post.content}</p>
+  <button class="text-blue-600">Beğen ({post.likes})</button>
+  <button class="text-blue-600">Hikayene Ekle</button>
+</div>
