a4b0c382625cde74ace50bb4185a393860a6ab35
Maraş Deri <marasderi@gmail.com>
2025-08-07 10:26:04 +0300

    Create Footer.svelte

---
+++ client/src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<footer class="bg-gray-800 text-white p-4 mt-8">
+  <div class="container mx-auto">
+    <div class="flex justify-between">
+      <div>
+        <h3 class="font-bold">Kamu Mikroblog</h3>
+        <p>© 2025 Tüm hakları saklıdır.</p>
+      </div>
+      <div>
+        <h3 class="font-bold">Bağlantılar</h3>
+        <ul>
+          <li><a href="/announcements" class="text-blue-300">Duyurular</a></li>
+          <li><a href="/contact" class="text-blue-300">İletişim</a></li>
+          <li><a href="/faq" class="text-blue-300">SSS</a></li>
+          <li><a href="/terms" class="text-blue-300">Kullanım Şartları</a></li>
+          <li><a href="/privacy" class="text-blue-300">Gizlilik Politikası</a></li>
+          <li><a href="https://twitter.com/kamumikroblog" class="text-blue-300">Twitter</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
