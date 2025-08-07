8bebe7fc2d830c11b99b31f7f04a4b22717f1acd
Maraş Deri <marasderi@gmail.com>
2025-08-07 10:29:11 +0300

    Create Story.svelte

---
+++ client/src/components/Story.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let story;
+</script>
+
+<div class="p-4 bg-white shadow rounded mb-4">
+  <p class="font-bold">{story.user.username}</p>
+  <p>{story.content}</p>
+  <p class="text-sm text-gray-500">Son {story.expiresAt}</p>
+</div>
